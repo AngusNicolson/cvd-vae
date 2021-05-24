5d92bc478e5478207a0d9e488c538e796634060f
Angus Nicolson <angusjnicolson@gmail.com>
2021-05-24 16:14:01 +0100

    Add calculation of weights for WeightedRandomSampler to ECGDataset

--- dataset.py
+++ dataset.py
@@ -9,12 +9,14 @@ from torch.utils.data import Dataset
 class ECGDataset(Dataset):
     """ECG dataset"""
 
-    def __init__(self, json_file: str, prefix: str = "", transform=None, load_ecg=True):
+    def __init__(self, json_file: str, prefix: str = "", transform=None, load_ecg=True, positive_ratio=1.0):
         """
         Args:
             json_file (string): Path to the json metadata file.
             prefix (string): Add a prefix to ECG file paths.
             transform (callable, optional): Optional transform to be applied on a sample.
+            load_ecg (bool): Whether to load ECG .npy arrays
+            positive_ratio (float): The average ratio of positive:negative samples per batch (for WeightedRandomSampler)
         """
         with open(json_file, "r") as fp:
             self.metadata = json.load(fp)
@@ -24,6 +26,12 @@ class ECGDataset(Dataset):
         self.pids = list(np.sort(list(self.metadata.keys())))
         self.ecg_type = "strip"
         self.load_ecg = load_ecg
+        self.classes = [v["cvd"]["incident"] for v in self.metadata.values()]
+        self.positive_ratio = positive_ratio
+        n_positive = sum(self.classes)
+        n_negative = len(self.classes) - sum(self.classes)
+        self.positive_weight = (positive_ratio*n_negative) / (n_positive + positive_ratio * n_negative)
+        self.weight = [self.positive_weight if v else 1 - self.positive_weight for v in self.classes]
 
     def __len__(self):
         return len(self.pids)
